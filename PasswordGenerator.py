85ac97102f5c1f3a5c29ae186e09e28cfed84f99
Saurabh Mukherjee <64585020+mesaum12@users.noreply.github.com>
2021-11-19 19:51:43 +0530

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,20 @@
+import string
+import random
+
+def passGen():
+    string1=string.ascii_uppercase
+    string2=string.ascii_lowercase
+    string3=string.digits
+    string4=string.punctuation
+    
+    passlen=int(input("Enter the length of the password to be set\n"))
+
+    s=[]
+    s.extend(list(string1))
+    s.extend(list(string2))
+    s.extend(list(string3))
+    s.extend(list(string4))
+    random.shuffle(s)
+    print(''.join(s[0:passlen]))
+
+passGen()
